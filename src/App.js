e251f4c4b921e98d3ea392af0d613e4bd6a312d8
Anderson Gonzalez <anderson199980@gmail.com>
2022-01-13 23:16:48 -0400

    primer commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+
 import './App.css';
 
+
+const Mensaje = () => {
+  return <h1>Hola mundo</h1>
+}
+
 function App() {
+
+  
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Titulo de la aplicación</h1>
+      <strong>Estamos trabajando en eso</strong>
+      <Mensaje></Mensaje>
     </div>
   );
 }
